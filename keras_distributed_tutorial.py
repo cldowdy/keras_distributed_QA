510faa00fb1c1e6741930cebddb5a7eb0532101a
Caleb Dowdy <cldowdy@eckerd.edu>
2023-06-06 11:15:37 -0400

    initial commit

---
+++ keras_distributed_tutorial.py
@@ -0,0 +1,47 @@
+import tensorflow_datasets as tfds
+import tensorflow as tf
+
+import os
+
+def scale(image, label):
+  image = tf.cast(image, tf.float32)
+  image /= 255
+
+  return image, label
+
+if __name__ == "__main__":
+    datasets, info = tfds.load(name='mnist', with_info=True, as_supervised=True)
+    mnist_train, mnist_test = datasets['train'], datasets['test']
+   
+   
+    strategy = tf.distribute.MirroredStrategy()
+
+    # You can also do info.splits.total_num_examples to get the total
+    # number of examples in the dataset.
+
+    num_train_examples = info.splits['train'].num_examples
+    num_test_examples = info.splits['test'].num_examples
+
+    BUFFER_SIZE = 10000
+
+    BATCH_SIZE_PER_REPLICA = 64
+    BATCH_SIZE = BATCH_SIZE_PER_REPLICA * strategy.num_replicas_in_sync
+
+    train_dataset = mnist_train.map(scale).cache().shuffle(BUFFER_SIZE).batch(BATCH_SIZE)
+    eval_dataset = mnist_test.map(scale).batch(BATCH_SIZE)
+
+    with strategy.scope():
+        model = tf.keras.Sequential([
+            tf.keras.layers.Conv2D(32, 3, activation='relu', input_shape=(28, 28, 1)),
+            tf.keras.layers.MaxPooling2D(),
+            tf.keras.layers.Flatten(),
+            tf.keras.layers.Dense(64, activation='relu'),
+            tf.keras.layers.Dense(10)
+        ])
+
+        model.compile(loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+                        optimizer=tf.keras.optimizers.Adam(),
+                        metrics=['accuracy'])
+        
+    EPOCHS = 12
+    model.fit(train_dataset, epochs=EPOCHS)
